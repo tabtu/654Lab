aa9e493e6b369bbbdf6d8df8d537b3338d3d7bab
Tab Tu <tabtu@ttxy.org>
2017-09-27 00:25:29 -0400

    create RedBlackBST

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a project built with IntelliJ IDEA 2017.
+The project is written for 654 Advanced Computing Concepts which include Labs and Assignments.
+Powered by Tab Tu individually.
--- src/Asmt1/A1_3.java
+++ src/Asmt1/A1_3.java
@@ -11,10 +11,36 @@ import Asmt1.hashTable.StringHashFamily;
  */
 public class A1_3 {
 
-    public static void main( String [ ] args ) {
+    public static CuckooHashTable<String> q1(int n) {
+        CuckooHashTable<String> Hc = new CuckooHashTable<>(new StringHashFamily(3), 2000);
+        long start, end;
+        double aver1, aver2;
+        start = System.currentTimeMillis();
+        for (int i = 0; i < n; i++) {
+            Hc.insert(ExtFuns.getRandomString(10));
+        }
+        end = System.currentTimeMillis();
+        long tsum = end - start;
+        System.out.println("The total insert time for " + n + " insertion is : " + tsum + "ms.");
+        return Hc;
+    }
 
-        final int NUMS = 2000000;
+    public static int q2(CuckooHashTable<String> hc, int n) {
+        long start, end;
+        int count = 0;
+        start = System.currentTimeMillis();
+        for (int i = 0; i < n; i++) {
+            if (hc.remove(ExtFuns.getRandomString(10))) {
+                count++;
+            }
+        }
+        end = System.currentTimeMillis();
+        long tsum = end - start;
+        System.out.println("The total random delete time is : " + tsum + "ms.");
+        return count;
+    }
 
+    public static void q3(int NUMS) {
         for (int k = 1; k < 20; k++) {
             System.out.println("Fill in the table...");
 
@@ -76,5 +102,13 @@ public class A1_3 {
             }
             System.out.println("Finished.");
         }
+    }
+
+    public static void main( String [ ] args ) {
+        final int n = 10000000;
+        final int NUMS = 2000000;
+
+        int delcount = q2(q1(n), n);
+        q3(NUMS);
     }
 }
