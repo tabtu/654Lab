04b217e2dc8fa79c29f0c4f49913e77a8d27e40d
Tab Tu <tabtu@ttxy.org>
2017-11-25 20:36:09 -0500

    complete assignment 4

--- src/uow/acc/tab/asmt4/tasks.java
+++ src/uow/acc/tab/asmt4/tasks.java
@@ -5,21 +5,21 @@ import uow.acc.tab.asmt4.utils.Func;
 
 public class tasks {
 
-    private final static String path = "data/";
+    private final static String DATA_PATH = "data/";
 
     public static void main(String[] args) {
-        //task1();
-        task2();
-        //task3();
-        //task4();
+        task1();
+//        task2();
+//        task3();
+//        task4();
+//        task5();
     }
 
     /**
      * Task1 in Assignment4, Create by Tab Tu, On Nov.14 2017
-     *
      */
     public static void task1() {
-        String txt = new In(path + "Hard disk.txt").readAll();
+        String txt = new In(DATA_PATH + "Hard disk.txt").readAll();
         String[] pats = {"hard", "disk", "hard disk", "hard drive", "hard dist", "xltpru"};
 
         long start;
@@ -68,24 +68,23 @@ public class tasks {
             kmp += (System.currentTimeMillis() - start);
         }
 
-        StdOut.println("BruteForceMatch: " + bf + "ms for " + N + " rounds and average CPU time for each searching is " + bf/N + "ms");
-        StdOut.println("BoyerMoore: " + bm + "ms for " + N + " rounds and average CPU time for each searching is " + bm/N + "ms");
-        StdOut.println("KMP: " + kmp + "ms for " + N + " rounds and average CPU time for each searching is " + kmp/N + "ms");
+        StdOut.println("BruteForceMatch: " + bf + "ms for " + N + " rounds and average CPU time for each searching is " + bf / N + "ms");
+        StdOut.println("BoyerMoore: " + bm + "ms for " + N + " rounds and average CPU time for each searching is " + bm / N + "ms");
+        StdOut.println("KMP: " + kmp + "ms for " + N + " rounds and average CPU time for each searching is " + kmp / N + "ms");
     }
 
     /**
      * Task2 in Assignment4, Create by Tab Tu, On Nov.14 2017
-     *
      */
     public static void task2() {
 
-        String[] key = { "protein", "complex", "PPI", "prediction", "physicochemical", "interface", "interactions", "complex", "individual", "predictions" };
-        String[] content = Func.getwords(new In(path + "Protein.txt").readAll());
+        String[] key = {"protein", "complex", "PPI", "prediction", "physicochemical", "interface", "interactions", "complex", "individual", "predictions"};
+        String[] content = Func.getwords(new In(DATA_PATH + "Protein.txt").readAll());
         TST<Integer> st = new TST<Integer>();
 
         // print keys
         StdOut.print("keys : ");
-        for(String each : key) {
+        for (String each : key) {
             StdOut.print(each + ", ");
         }
         StdOut.println();
@@ -100,18 +99,105 @@ public class tasks {
             StdOut.println("key = shells, value = " + st.get(each));
         }
         StdOut.println("It cost " + time + "ms to find these " + key.length + " keys");
-        StdOut.println("So, average time for each round is " + time/key.length + "ms");
+        StdOut.println("So, average time for each round is " + time / key.length + "ms");
     }
 
     /**
-     * Task3 in Assignment3, Create by Tab Tu, On Nov.14 2017
-     *
+     * Task3 in Assignment4, Create by Tab Tu, On Nov.21 2017
      */
     public static void task3() {
-
+        String sourcepath = DATA_PATH + "W3C Web Pages/";
+        String targetpath = DATA_PATH + "asmt4text/";
+        String[] list = Func.getFilesFromPath(sourcepath);
+        for (String each : list) {
+            String tmp = Func.readFile2String(sourcepath + each);
+            String tmot = each.substring(0, each.length() - 4) + ".txt";
+            Func.writeString2File(targetpath + tmot, tmp);
+        }
+        StdOut.println("Completed transfer " + list.length + " files into " + targetpath);
     }
 
+    /**
+     * Task4 in Assignment4, Create by Tab Tu, On Nov.21 2017
+     */
     public static void task4() {
+        String path = DATA_PATH + "asmt4text/";
+        String[] list = Func.getFilesFromPath(path);
 
+        String ppn = "(\\()?(\\d){3}(\\))?[- ](\\d){3}-(\\d){4}";
+        String reppn = "";
+        String pea = "[\\w]+@[\\w]+\\.[com|net|ca|cn|cc|org]";
+        String repea = "";
+
+        for (String each : list) {
+            String tmp = Func.readFile2String(path + each);
+            String phonenumber = Func.findByPattern(ppn, tmp);
+            String emailaddress = Func.findByPattern(pea, tmp);
+            if (phonenumber.length() > 0) {
+                reppn += "\n\nFile name: " + each + ": ";
+                reppn += phonenumber;
+            }
+            if (emailaddress.length() > 0) {
+                repea += "\n\nFile name: " + each + ": ";
+                repea += emailaddress;
+            }
+        }
+
+        StdOut.print("\n------------Find Phone Number: ------------");
+        StdOut.println(reppn);
+        StdOut.print("\n\n------------Find Email Address: ------------");
+        StdOut.println(repea);
+    }
+
+    /**
+     * Task5 in Assignment4, Create by Tab Tu, On Nov.21 2017
+     */
+    public static void task5() {
+        String path = DATA_PATH + "W3C Web Pages/";
+        String[] list = Func.getFilesFromPath(path);
+
+        //String urlReg = "<a href\\s?=\\s?\"(http://|https://)?(www.)?([a-zA-Z0-9]+)\\.[a-zA-Z0-9]*\\.?[a-zA-Z]{2,3}.?([a-z]+)?\"\\s?>";
+        String w3Rega = "<a href\\s?=\\s?\"(http://|https://)?(www.)?(w3.org)\"\\s?>";
+        String rea = "";
+        String w3Regb = "<a href\\s?=\\s?\"(http://|https://)?(www.)?([a-zA-Z0-9]+)\\.[a-zA-Z0-9]*\\.?[a-zA-Z]{2,3}.?([a-z]+)?(/[a-zA-Z0-9]+)+\"\\s?>";
+        String reb = "";
+        String w3Regc = "<a href\\s?=\\s?\"(http://|https://)?(www.)?([a-zA-Z0-9]+)\\.[a-zA-Z0-9]*\\.?[a-zA-Z]{2,3}.?([a-z]+)?(/[a-zA-Z0-9]+)*(/#[a-zA-Z0-9]+)+\"\\s?>";
+        String rec = "";
+        String w3Regd = "<a href\\s?=\\s?\"(http://|https://)?(www.)?([a-zA-Z0-9]+).[a-zA-Z0-9]*.(.net|.com|.org)\"\\s?>";
+        String red = "";
+
+        for (String each : list) {
+            String tmp = Func.readFile2String(path + each);
+            String tmpa = Func.findByPattern(w3Rega, tmp);
+            String tmpb = Func.findByPattern(w3Regb, tmp);
+            String tmpc = Func.findByPattern(w3Regc, tmp);
+            String tmpd = Func.findByPattern(w3Regd, tmp);
+            if (tmpa.length() > 0) {
+                rea += "\n\nFile name: " + each + ": ";
+                rea += tmpa;
+            }
+            if (tmpb.length() > 0) {
+                reb += "\n\nFile name: " + each + ": ";
+                reb += tmpb;
+            }
+            if (tmpc.length() > 0) {
+                rec += "\n\nFile name: " + each + ": ";
+                rec += tmpc;
+            }
+            if (tmpd.length() > 0) {
+                red += "\n\nFile name: " + each + ": ";
+                red += tmpd;
+            }
+        }
+
+        StdOut.print("------------Find links with domain w3.org: ------------");
+        StdOut.println(rea);
+        StdOut.print("------------Find links that contain folders: ------------");
+        StdOut.println(reb);
+        StdOut.print("------------Find links that contain references: ------------");
+        StdOut.println(rec);
+        StdOut.print("------------Find links with domain .net, .com, .org: ------------");
+        StdOut.println(red);
     }
 }
+
--- src/uow/acc/tab/asmt4/utils/Func.java
+++ src/uow/acc/tab/asmt4/utils/Func.java
@@ -1,9 +1,25 @@
 package uow.acc.tab.asmt4.utils;
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Out;
+import org.jsoup.Jsoup;
+import uow.acc.tab.asmt4.language.htmlparser.HTMLtoText;
+
+import javax.xml.crypto.Data;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * Functions in Assignment 4
+ *
+ * @Author Tab Tu
+ * @Updated Nov.23 2017
+ */
 public class Func {
     public static String[] getwords(String allcontent) {
         String regEx="[^a-z0-9]";
@@ -20,5 +36,74 @@ public class Func {
             result[i] = stmp.get(i);
         }
         return result;
+    }
+
+    public static String findByPattern(String pattern, String content) {
+        String result = "";
+        Pattern r = Pattern.compile(pattern);
+        Matcher m = r.matcher(content);
+        while (m.find( )) {
+            result += "\n" + m.group() + " at " + m.start();
+        }
+        return result;
+    }
+
+    public static String readFile2String(String filename) {
+        In in = new In(filename);
+        return in.readAll();
+    }
+
+    public static void writeString2File(String filename, String data) {
+        Out out = new Out(filename);
+        out.print(data);
+    }
+
+    public static String[] getFilesFromPath(String path) {
+        File[] files = new File(path).listFiles();
+        List<String> tmp = new ArrayList<>();
+        for (File file : files) {
+            if(!file.isDirectory()) {
+                if(file.getName().compareTo(".DS_Store") != 0) {
+                    tmp.add(file.getName());
+                }
+            }
+        }
+        String[] result = new String[tmp.size()];
+        return tmp.toArray(result);
+    }
+
+    public static String convertHTMLfile2String(String htmlfilename) {
+        try {
+            FileReader in = new FileReader(htmlfilename);
+            HTMLtoText parser = new HTMLtoText();
+            parser.parse(in);
+            in.close();
+            return parser.getText();
+        } catch(IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static String getHTMLfileStringFromURL(String url, String path) {
+        try {
+            org.jsoup.nodes.Document doc = Jsoup.connect(url).get();
+            String text = doc.text();
+            String filename = UUID.randomUUID().toString();
+
+            PrintWriter out = new PrintWriter(path + filename + ".txt");
+            out.println(text);
+            out.close();
+
+            String html = doc.html();
+            out = new PrintWriter( path + filename + ".html");
+            out.println(html);
+            out.close();
+
+            return filename;
+        } catch(IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
